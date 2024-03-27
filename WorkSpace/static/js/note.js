9d26b1cdfcc5fa44cfdf4cf736401bda0b91e5bd
Anuj Tiwari <135116612+anxenown@users.noreply.github.com>
2024-03-27 23:00:14 +0530

    update

--- WorkSpace/static/js/note.js
+++ WorkSpace/static/js/note.js
@@ -1,40 +1,49 @@
 // save | allnotes
-let inputTitle= document.getElementById("note-title");
-let saveBtn= document.getElementById("saveBtn");
-let inputText= document.getElementById("textarea");
-let output= document.getElementById("output");
+let inputTitle = document.getElementById("note-title");
+let saveBtn = document.getElementById("saveBtn");
+let inputText = document.getElementById("textarea");
+let output = document.getElementById("output");
 
-let child, delBtn, h2, p;
+let child, favBtn, delBtn, h2, p;
 
-saveBtn.addEventListner('click', function(e){
+saveBtn.addEventListener('click', (e) => { 
     e.preventDefault();
-    if ((inputTitle.value == "") || (inputText.value == ""))
-    {
-        alert("Please enter some Title and Text");}
-    else{
-      child =document.createElement("div");
-      delBtn =document.createElement("button");
-      h2 =document.createElement("h2");  
-      p =document.createElement("p"); 
+    if (inputTitle.value == "" || inputText.value == "") {
+        alert("Please enter some Title and Text");
+    } else {
+        child = document.createElement("div");
+        favBtn = document.createElement("button");
+        delBtn = document.createElement("button");
+        h2 = document.createElement("h2");
+        p = document.createElement("p");
 
-      h2.innerHTML = inputTitle.value;
-      p.innerHTML = inputText.value;
-      delBtn.innerHTML = "Delete";
+        h2.innerHTML = inputTitle.value;
+        p.innerHTML = inputText.value;
+        delBtn.innerHTML = "Delete";
+        favBtn.innerHTML = "☆";
 
-      child.classList.add("list");
-      delBtn.classList.add("delete");
-      h2.classList.add("output-title");
-      p.classList.add("text");
+        child.classList.add("list");
+        favBtn.classList.add("fav");
+        delBtn.classList.add("delete");
+        h2.classList.add("output-title");
+        p.classList.add("text");
+        
+        
+        child.appendChild(delBtn); // This ensures the delete button is added after the fav button
+        child.appendChild(favBtn);
+        child.appendChild(h2);
+        child.appendChild(p);
+        output.appendChild(child);
 
-      child.appendChild(delBtn);
-      child.appendChild(h2);
-      child.appendChild(p);
-      output.appendChild(child);
-
-      inputTitle.innerHTML= "";
-      inputText.innerHTML= "";
-    }    
-     delBtn.addEventListner('click', function(){
-         output.removeChild(this.parentElement);
-     })
+        // clear input fields
+        inputTitle.value = "";
+        inputText.value = "";
+    }
+    favBtn.addEventListener("click", () => {
+        favBtn.classList.toggle("color");
     })
+    delBtn.addEventListener('click', function() {
+        output.removeChild(this.parentElement);
+    });
+});
+
